3226b87639c30d0089fa688912ac0b7577c4ada7
Anas Benali <azayz9831@gmail.com>
2026-04-20 15:09:27 +0200

    feat: add generate_usernames and check_username functions

--- modules/username_checker.py
+++ modules/username_checker.py
@@ -1,5 +1,6 @@
 import hashlib
 import requests
+from outils.email_parser import parse_email
 def hash_password(password):
     """
     Hache un mot de passe en SHA-1.
@@ -58,4 +59,122 @@ def check_password(password):
         
 
 
+def generate_username(mail):
+    assert isinstance (mail,str),'mail doit etre un str'
+    res = []
+    separate = 0
+    username = parse_email(mail)["name"]
+    if "-" in username:
+        separate = username.find("-")
+    elif "." in username:
+        separate = username.find(".")
+    elif "_" in username:
+        separate = username.find("_")
+    
+    if separate == 0:
+        res.append(username)
+    else:
+        part1 = username[:separate]
+        part2 = username[separate+1:]
+        res.append(part1)
+        res.append(part2)
+        res.append(username)
+        res.append(part1[0]+part2)
+        res.append(part2[0]+part1)
+        res.append(part1+part2[0])
+        res.append(part2+part1[0])
+        #avec un .
+        res.append(part1[0]+"."+part2)
+        res.append(part2[0]+"."+part1)
+        res.append(part1+"."+part2[0])
+        res.append(part2+"."+part1[0])
+        res.append(part1+"."+part2)
+        res.append(part2+"."+part1)
+        #avec un _
+        res.append(part1[0]+"_"+part2)
+        res.append(part2[0]+"_"+part1)
+        res.append(part1+"_"+part2[0])
+        res.append(part2+"_"+part1[0])
+        res.append(part1+"_"+part2)
+        res.append(part2+"_"+part1)
+        #avec un -
+        res.append(part1[0]+"-"+part2)
+        res.append(part2[0]+"-"+part1)
+        res.append(part1+"-"+part2[0])
+        res.append(part2+"-"+part1[0])
+        res.append(part1+"-"+part2)
+        res.append(part2+"-"+part1)
+    return res
+
+def check_username(username):
+    assert isinstance (username,str),'username doit être un  str'
+    d = {
+        "username": username,
+        "results":{}
+    }
+    reponse_instagram = requests.get("https://www.instagram.com/" + username)
+    reponse_tiktok = requests.get("https://www.tiktok.com/@"+ username)
+    reponse_x = requests.get("https://x.com/" + username) 
+    reponse_reddit = requests.get("https://www.reddit.com/user/"+username)
+    reponse_github = requests.get("https://github.com/"+username)
+    reponse_youtube = requests.get("https://www.youtube.com/@"+username)
+    reponse_linkedin = requests.get("https://www.linkedin.com/in/"+username)
+    reponse_snapchat = requests.get("https://www.snapchat.com/add/"+username)
+    reponse_twitch = requests.get("https://www.twitch.tv/"+username)
+    reponse_steam = requests.get("https://steamcommunity.com/id/"+username)
+    reponse_pinterest = requests.get("https://www.pinterest.com/"+username)
+    t = [reponse_github,reponse_instagram,reponse_reddit,reponse_tiktok,reponse_x,reponse_youtube,reponse_linkedin,reponse_snapchat,reponse_twitch,reponse_instagram,reponse_pinterest,reponse_steam]
+    for reponse in t:
+        if reponse.status_code == 200:
+            if "instagram" in reponse.url:
+                d["results"]["instagram"] = True
+            elif "tiktok" in reponse.url:
+                d["results"]["tiktok"] = True
+            elif "x" in reponse.url:
+                d["results"]["x"] = True
+            elif "reddit" in reponse.url:
+                d["results"]["reddit"] = True
+            elif "github" in reponse.url:
+                d["results"]["github"] = True
+            elif "youtube" in reponse.url:
+                d["results"]["youtube"] = True
+            elif "linkedin" in reponse.url:
+                d["results"]["linkedin"] = True
+            elif "snapchat" in reponse.url:
+                d["results"]["snapchat"] = True
+            elif "twitch" in reponse.url:
+                d["results"]["twitch"] = True
+            elif "steam" in reponse.url:
+                d["results"]["steam"] = True
+            elif "pinterest" in reponse.url:
+                d["results"]["pinterest"] = True
+
+        else:
+            if "instagram" in reponse.url:
+                d["results"]["instagram"] = False
+            elif "tiktok" in reponse.url:
+                d["results"]["tiktok"] = False
+            elif "x" in reponse.url:
+                d["results"]["x"] = False
+            elif "reddit" in reponse.url:
+                d["results"]["rediit"] = False
+            elif "github" in reponse.url:
+                d["results"]["github"] = False
+            elif "youtube" in reponse.url:
+                d["results"]["youtube"] = False
+            elif "linkedin" in reponse.url:
+                d["results"]["linkedin"] = False
+            elif "snapchat" in reponse.url:
+                d["results"]["snapchat"] = False
+            elif "twitch" in reponse.url:
+                d["results"]["twitch"] = False
+            elif "steam" in reponse.url:
+                d["results"]["steam"] = False
+            elif "pinterest" in reponse.url:
+                d["results"]["pinterest"] = False
+    
+    return d
+
+
+    
 
